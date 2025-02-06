66ece183f0331f9ec82eab4c0fb504e1b8df9123
silencecoder3000 <carlos.gallegos03@epn.edu.ec>
2025-02-05 21:23:03 -0500

    feat(MecanicoEntity): Cambios en CRUD de la entidad mecanico

--- build/models/MecanicoModel.py
+++ build/models/MecanicoModel.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DATETIME, Identity
+from sqlalchemy import Column, Integer, String, ForeignKey, DATETIME, Table, MetaData
 from build.models.Base import Base
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
 
 class Mecanico(Base):
     __tablename__ = 'Mecanico'
-    __table_args__ = {"schema": "dbo"}
+
     mecanico_id = Column(Integer,primary_key=True)
     id_taller = Column(Integer, nullable=False)
     especialidad = Column(String, nullable=False)
     nombre_completo_mecanico = Column(String, nullable=False)
-
--- build/service/MecanicoEntity.py
+++ build/service/MecanicoEntity.py
@@ -1,6 +1,9 @@
 from sqlalchemy.orm import Session
 from build.models.MecanicoModel import Mecanico
 from build.service.ConexionQuito import get_db
+from sqlalchemy import text
+
+
 
 
 class MecanicoEntity:
@@ -8,29 +11,44 @@ class MecanicoEntity:
     @staticmethod
     def crear_mecanico(mecanico_id: int,id_taller: int, especialidad: str, nombre_completo_mecanico: str):
         db = next(get_db())
-        nuevo_mecanico = Mecanico(mecanico_id=mecanico_id,id_taller=id_taller, especialidad=especialidad, nombre_completo_mecanico=nombre_completo_mecanico)
-        db.add(nuevo_mecanico)
-        db.commit()
-        db.refresh(nuevo_mecanico)
-        return nuevo_mecanico
+        query = text("""
+            INSERT INTO Mecanico (mecanico_id, id_taller, especialidad, nombre_completo_mecanico)
+            VALUES (:mecanico_id, :id_taller, :especialidad, :nombre_completo_mecanico)
+            """)
+        db.execute(query, {
+        'mecanico_id': mecanico_id,
+        'id_taller': id_taller,
+        'especialidad': especialidad,
+        'nombre_completo_mecanico': nombre_completo_mecanico
+    })
+        db.commit()  # Asegúrate de hacer commit si estás realizando una operación que cambia datos.
 
     def obtener_mecanicos(self):
         return self.db.query(Mecanico).all()
 
-    def actualizar_mecanico(self, mecanico_id: int, especialidad: str = None, nombre_completo_mecanico: str = None):
-        mecanico = self.db.query(Mecanico).filter(Mecanico.mecanico_id == mecanico_id).first()
+    def actualizar_mecanico( mecanico_id: int = None, id_taller: int = None  ,especialidad: str = None, nombre_completo_mecanico: str = None):
+        db = next(get_db())
+        mecanico = db.query(Mecanico).filter(Mecanico.mecanico_id == mecanico_id).first()
         if mecanico:
             if especialidad is not None:
                 mecanico.especialidad = especialidad
             if nombre_completo_mecanico is not None:
                 mecanico.nombre_completo_mecanico = nombre_completo_mecanico
-            self.db.commit()
-            self.db.refresh(mecanico)
+            if mecanico_id is not None:
+                mecanico.mecanico_id = mecanico_id
+            if id_taller is not None:
+                mecanico.id_taller = id_taller
+            db.commit()
+            db.refresh(mecanico)
         return mecanico
 
-    def eliminar_mecanico(self, mecanico_id: int):
-        mecanico = self.db.query(Mecanico).filter(Mecanico.mecanico_id == mecanico_id).first()
-        if mecanico:
-            self.db.delete(mecanico)
-            self.db.commit()
-        return mecanico
+    @staticmethod
+    def eliminar_mecanico(mecanico_id: int):
+        db = next(get_db())
+        query = text("""
+            SET XACT_ABORT ON;
+            DELETE FROM Mecanico
+            WHERE mecanico_id = :mecanico_id
+        """)
+        db.execute(query, {'mecanico_id': mecanico_id})
+        db.commit()  # Aseguramos que el cambio sea persistido.
